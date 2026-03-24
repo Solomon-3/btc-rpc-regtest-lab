480a735e6f98e26650fc470d4e2dbe218737729f
Spencer <solomonjass4@gmail.com>
2026-03-24 20:08:26 +0300

    day 2, finished day 2 exercises, decode, blockchaininfo, wallet balance, list transactions, block details,embed message on-chain

--- day2/explore_starter.py
+++ day2/explore_starter.py
@@ -67,18 +67,19 @@ print(f"=======================================")
 
 # execise 4
 count = 5
+wallet_name="spencer"
 def list_transactions(wallet_name, count=5):
-  url = "http://127.0.0.1:18443/"
+ url = "http://127.0.0.1:18443/"
 
 try:
    rpc("loadwallet", [wallet_name])
-except Exception:
+except Exception :
    pass
 
-txs = rpc("list_transactions", ["*",count], wallet = spencer)
+txs = rpc("list_transactions", ["*",count], wallet=wallet_name)
 
 print(f"====== exercise 4 =======")
-print(f"=== Last{count} transactions ({wallet_name}) ===")
+print(f"=== Last {count} transactions ({wallet_name}) ===")
 
 for tx in txs:
    if tx['category'] in ('receive','generate'):
@@ -88,4 +89,60 @@ for tx in txs:
    print(f" {direction} {tx['amount']:+.8f} BTC")
    print(f" txid: {tx['txid'][:32]}...")
 
-# exercise 5
+
+
+# exercise 5 DECODE TRANSACTION
+print(f"==== DAY 2 exercise 5 decode transaction ========")
+
+def decode_transaction(txid):
+  tx = rpc("getrawtransaction", [txid, True])
+  print(f"=== Transaction ({tx['size']} bytes ===)")
+
+  print("\nInputs:")
+  for vin in tx['vin']:
+      if 'coinbase' in vin:
+           print("COINBASE (mining reward)")
+      else:
+           print(f" From: {vin['txid'][:24]}...")
+
+  print("\nOutputs:")
+  for vout in tx['vout']:
+     addr = vout['scriptPubKey'].get('address', '?')
+     print(f" {vout['value']:.8f} BTC -> {addr}")
+
+
+
+# exercise 6  BLOCK DETAILS
+print(f"=== Day 2 execise 6 block detais")
+
+def show_block(blockhash=None):
+    if blockhash is None:
+        blockhash = rpc("getbestblockhash")
+    block =rpc("getblock", [blockhash, 1])
+
+    print(f"=== Block {block['height']} ===")
+    print(f" Hash: {block['hash'][:32]}...")
+    print(f" Time: {block['time']}")
+    print(f" TXs: {block['nTx']}")
+    print(f" Size: {block['size']} bytes")
+
+
+# exercise 7  Embedded a message on-chain
+print(f"=== day 2 exercise 7 embedded a message on-chain")
+
+def embed_message(wallet_name, message):
+   hex_msg = message.encode().hex()
+   utxos = rpc("listunspent", [1], wallet=wallet_name)
+   utxo = utxos[0]
+   change = round(utxo['amount'] - 0.0001, 8)
+   change_addr = rpc("getnewaddress", wallet=wallet_name)
+   raw = rpc("createrawtransaction", [[{"txid": utxo[txid], "vout": utxo['vout']}], [{"data": hex_msg}, {change_addr: change}])
+   signed = rpc("signrawtransactionwithwallet", [raw], wallet=wallet_name)
+   txid = rpc("sendrawtransaction", [signed['hex']])
+   print(f"Message '{message}' embedded! TX: {txid[:32]}...")
+
+
+
+# exercise 8 MULTISIG
+#print(f"=== day 2 exercise 8 multisig")
+
